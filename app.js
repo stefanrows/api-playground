f17c4f5a09ad3089e30ad2a2b06c7864869e5f1e
stefanrows <stefan.rosanitsch@gmail.com>
2020-11-01 23:32:57 +0100

    Fix: Made submit to search work

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
 //Simple API call using fetch & displaying data in the DOM
 
 const getJoke = () => {
-	fetch('https://api.chucknorris.io/jokes/random').then((response) => response.json()).then((data) => {
-		let text = data.value;
-		const p = document.createElement('p');
-		p.textContent = data.value;
-		document.getElementById('randomJokeDisplay').appendChild(p);
-	});
+  fetch('https://api.chucknorris.io/jokes/random')
+    .then((response) => response.json())
+    .then((data) => {
+      let text = data.value;
+      const p = document.createElement('p');
+      p.textContent = data.value;
+      document.getElementById('randomJokeDisplay').appendChild(p);
+    });
 };
 
 const getPokemon = () => {
-	const selectForm = document.querySelector('#input');
-	const submitButton = document.querySelector('#submit');
+  const pokeSearchForm = document.querySelector('.pokemon-search');
+  const inputField = document.querySelector('#input-field');
 
-	pokeFilter = '';
+  pokeFilter = '';
 
-	selectForm.addEventListener('change', function(e) {
-		e.preventDefault();
-		pokeFilter = e.target.value;
-		console.log(pokeFilter);
-		url = `https://pokeapi.co/api/v2/pokemon/${pokeFilter}`;
-		console.log(url);
-		fetch(`https://pokeapi.co/api/v2/pokemon/${pokeFilter}`).then((response) => response.json()).then((data) => {
-			console.log(data);
-			// Card Img
-			const img = document.createElement('img');
-			img.src = data.sprites.front_default;
-			document.querySelector('.poke-img').appendChild(img);
-			//Card Title
-			pokemonName = data.name;
-			const nameCapitalized = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
-			const h3 = document.createElement('h3');
-			h3.textContent = nameCapitalized;
-			document.querySelector('.poke-title').appendChild(h3);
+  pokeSearchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    pokeFilter = inputField.value;
 
-			//Card Text
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeFilter}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Card Img
+        const img = document.createElement('img');
+        img.src = data.sprites.front_default;
+        document.querySelector('.poke-img').appendChild(img);
+        //Card Title
+        pokemonName = data.name;
+        const nameCapitalized =
+          pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+        const h3 = document.createElement('h3');
+        h3.textContent = nameCapitalized;
+        document.querySelector('.poke-title').appendChild(h3);
 
-			//Card Button
-		});
-	});
+        //Card Text
+
+        //Card Button
+
+        // Clear everything
+      });
+  });
 };
 
 getJoke();

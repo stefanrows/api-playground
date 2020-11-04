e45ba6a3fdb74052a8e79c66b22fcb299626e624
Stefan Rows <38349705+stefanrows@users.noreply.github.com>
2020-11-05 00:13:36 +0100

    Feature/Dropdown
    * Add DropDown Menu
    * Populate DropDown Menu with Pokemon by fetching API JSON Data

--- app.js
+++ app.js
@@ -14,7 +14,43 @@ const getJoke = () => {
 const getPokemon = () => {
   const pokeSearchForm = document.querySelector('.pokemon-search');
   const inputField = document.querySelector('#input-field');
+  const dropdown = document.querySelector('#locality-dropdown');
 
+  // DropDown Code
+  const dropdownURL = 'https://pokeapi.co/api/v2/pokemon';
+  dropdown.length = 0;
+
+  let defaultOption = document.createElement('option');
+  defaultOption.text = 'Choose a Pokemon';
+
+  dropdown.add(defaultOption);
+  dropdown.selectedIndex = 0;
+
+  fetch(dropdownURL)
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.warn(
+          `Looks like there was a problem. Status Code: ${response.status}`,
+        );
+        return;
+      }
+      // Examine the text in the response
+      response.json().then(function (data) {
+        let option;
+
+        for (let i = 0; i < data.results.length; i++) {
+          option = document.createElement('option');
+          option.text = data.results[i].name;
+          option.value = data.results[i].name;
+          dropdown.add(option);
+        }
+      });
+    })
+    .catch(function (err) {
+      console.error('Fetch Error -', err);
+    });
+
+  // Search Field Code
   pokeFilter = '';
 
   pokeSearchForm.addEventListener('submit', function (e) {
@@ -47,5 +83,15 @@ const getPokemon = () => {
   });
 };
 
+// Testing
+
+const pokeTest = () => {
+  fetch('https://pokeapi.co/api/v2/pokemon/')
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
+};
+
 getJoke();
 getPokemon();
+pokeTest();
